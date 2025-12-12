5b9dd685ed580924b45b909cbd65de2969db5211
akinloluwami <akinkunmioye42@gmail.com>
2025-12-12 22:48:40 +0100

    chore: add nitro package and integrate with Vite config

--- apps/web/vite.config.ts
+++ apps/web/vite.config.ts
@@ -3,10 +3,17 @@ import viteReact from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import tsConfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [tsConfigPaths(), tanstackStart(), viteReact(), tailwindcss()],
+  plugins: [
+    tsConfigPaths(),
+    tanstackStart(),
+    nitro(),
+    viteReact(),
+    tailwindcss(),
+  ],
   server: {
     allowedHosts: true,
   },
